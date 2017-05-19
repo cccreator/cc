3785c286c8a408b8295e5643158400907727276b
cccreator <13194239765@163.com>
2017-05-19 10:41:30 +0800

    add

---
+++ qqChatSystem/sql and make/insert.sql
@@ -0,0 +1,5 @@
+
+insert into baidu (ID, url, name, description) values (seq1.nextval, 'www.sina.com', 'sina新浪', '新浪的首页');
+insert into baidu (ID, url, name, description) values (seq1.nextval, 'www.itcast.cn');
+insert into baidu (ID, url, name, description) values (seq1.nextval, '192.168.1.254', 'baidu百度', '百度的首页');
+commit;
---
+++ qqChatSystem/sql and make/myoracle.h
@@ -0,0 +1,9 @@
+﻿
+#ifndef MYORACLE_H_
+#define MYORACLE_H_
+
+int sql_connect(const char *User, const char *Password, const char *DBName);
+int sql_disconnect();
+int query_result(const char *name, char **buf);
+
+#endif /* MYORACLE_H_ */
